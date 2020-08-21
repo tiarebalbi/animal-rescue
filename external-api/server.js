c09afdb3984bbe178a37ebd4f6386df50c6876e7
Ollie Hughes <ohughes@pivotal.io>
2020-08-21 16:07:28 +0100

    starting point for demo

--- external-api/server.js
+++ external-api/server.js
@@ -1,25 +1,32 @@
 const http = require('http');
 const axios = require('axios');
 
-const animalRescueBaseUrl = process.env.ANIMAL_RESCUE_BASE_URL;
+const animalRescueBaseUrl = process.env.ANIMAL_RESCUE_BASE_URL
+const animalRescueUsername = process.env.ANIMAL_RESCUE_USERNAME || ''
+const animalRescuePassword = process.env.ANIMAL_RESCUE_PASSWORD || ''
 
 const requestAnimalsFromAnimalRescue = async () => {
     try {
         const response = await axios.get(`${animalRescueBaseUrl}/api/animals`);
-        return response.data;
+        return { animals: response.data };
     } catch (e) {
         console.error(e);
+        return { error: e };
     }
-};
+}
 
 const server = http.createServer(async (req, res) => {
     if (req.url === '/') {
 
-        res.writeHead(200, {'Content-Type': 'text/html'});
 
         const animals = await requestAnimalsFromAnimalRescue()
-        const animalHtmlList = animals.map(animal => `<li>${animal.name}</li>`).join('')
-        res.write(`<html><body><p>Animals available at Animal Rescue: ${animalHtmlList}</body></html>`);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        if(animals.error) {
+            res.write(`<html><body><p>Failed to retrieve animals: ${animals.error}</body></html>`);
+        } else {
+            const animalHtmlList = animals.map(animal => `<li>${animal.name}</li>`).join('')
+            res.write(`<html><body><p>Animals available at Animal Rescue: ${animalHtmlList}</body></html>`);
+        }
         res.end();
 
     }
